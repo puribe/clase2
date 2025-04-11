086bf6028639c3a5a003e5ad88115d588ff9a9b9
puribe <puribe@puribe.cl>
2025-04-10 20:05:39 -0400

    tests

--- myapp/tests.py
+++ myapp/tests.py
@@ -10,3 +10,7 @@ class PingTests(TestCase):
         response = self.client.get('/ping/')
         self.assertEqual(response.status_code,200)
         self.assertJSONEqual(response.content, {'ping': 'pong'})
+
+    def test_endpoint_ping_responde_404(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 404)
